0e21588f6de5895df1995e46918e1730223fb4a4
suk-6 <me@suk.kr>
2024-03-11 12:01:22 +0900

    Update version and package inclusion in setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,22 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    install_requires = fh.read().splitlines()
+
 setuptools.setup(
     name="ovcontrolnet_tools",
-    version="1.0.0",
+    version="1.0.1",
     author="suk-6",
     author_email="me@suk.kr",
     description="Packages for simple implementations of OVControlNet",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/suk-6/ovcontrolnet-tools",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(
+        include=["ovcontrolnet_tools", "ovcontrolnet_tools.*"]
+    ),
+    install_requires=install_requires,
     python_requires=">=3.6",
     license="MIT",
 )
